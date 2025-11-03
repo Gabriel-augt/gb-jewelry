baba327d49018974578281623e172955e4bb3abe
Gabriel Augusto <gabrielaugustocfal@gmail.com>
2025-11-03 18:11:14 -0400

    changed hero

--- src/app/components/homePage/Hero.tsx
+++ src/app/components/homePage/Hero.tsx
@@ -2,31 +2,41 @@ import Image from "next/image";
 import HeroButton from "./HeroButton";
 
 export default function Hero() {
-    return (
-        <main>
-            <section className="flex justify-evenly items-center w-full h-screen bg-white
-            max-xl:flex-col max-sm:gap-0 max-xl:h-max max-xl:gap-28 max-xl:mb-20">
-                <div className="flex flex-col justify-center items-center text-black">
-                    <div className="relative w-[500px] h-[500px] max-sm:w-96">
-                    <Image src={"/flower-design-1.svg"} alt={"Flower Design"} fill/>
-                    </div>
-                    <div className="absolute text-center font-inter text-lg px-6 min-[520px]:w-[500px]">
-                        <span className="max-sm:text-sm">Vestibulum aliquam porta eros</span>
-                        <h1 className="font-cormorant font-medium text-6xl
-                        max-sm:text-3xl max-sm:font-bold">
-                        Joias únicas, pedras preciosas raras e peças requintadas!
-                        </h1>
-                        <p className="font-eb-garamond font-light mt-8 text-left max-sm:text-base max-sm:mt-6">
-                            Aqui, oferecemos diversos tipos de joias, incluindo colares, pulseiras, brincos e anéis. De designs clássicos a estilos modernos, temos algo para todos os gostos.
-                        </p>
-                        <HeroButton />
-                    </div>
-                </div>
-                <div className="relative border border-zinc-200 w-[456px] h-[656px] rounded-t-full max-sm:w-72 max-sm:h-[450px]">
-                    <Image className="p-8 rounded-t-full max-sm:p-4" src={"/model-hero.jpg"} alt={"Model"} fill/>
-                    <Image className="translate-y-64 -translate-x-12" src={"/star.svg"} alt={"Model"} width={80} height={80}/>
-                </div>
-            </section>
-        </main>
-    )
+  return (
+    <main className="inline-flex items-center justify-evenly h-screen w-full gap-16
+    max-xl:flex-col max-xl:justify-center min-xl:gap-28 max-md:h-[860px]">
+      <div className="flex flex-col text-black w-max px-6 max-md:w-full">
+        <h1 className="text-7xl font-cormorant font-semibold uppercase text-shadow-md
+        w-96 max-sm:text-5xl min-lg:text-8xl">
+          Sinta o Glamour
+        </h1>
+        <span
+          className="font-inter font-light text-sm text-shadow-sm pt-6
+          max-sm:text-sm max-sm:w-80">
+          Abrace o glamour e revele seu brilho interior com nossas criações exclusivas.
+        </span>
+        <HeroButton />
+      </div>
+      <div className="inline-flex gap-6 max-sm:gap-3">
+        <div className="relative w-44 h-96 translate-y-20 min-md:w-60
+        min-md:h-96 min-xl:w-[364px] min-xl:h-[600px] max-sm:h-80">
+        <Image
+          className="object-cover"
+          src="/hero-2.jpg"
+          alt="Earring"
+          fill
+        />
+      </div>
+      <div className="relative w-44 h-96 min-md:w-60 min-md:h-96
+      min-xl:w-[364px] min-xl:h-[600px] max-sm:h-80">
+        <Image
+          className="object-cover"
+          src="/hero-1.jpg"
+          alt="Woman with jewelry"
+          fill
+        />
+      </div>
+      </div>
+    </main>
+  );
 }
--- src/app/components/homePage/HeroButton.tsx
+++ src/app/components/homePage/HeroButton.tsx
@@ -4,11 +4,10 @@ import Link from "next/link";
 export default function HeroButton () {
     return (
         <Link href='#shop'>
-            <button className="absolute flex flex-row gap-2 justify-center items-center py-4 px-6 border
-            border-zinc-300 rounded-br-2xl rounded-l-2xl font-eb-garamond translate-y-20 group cursor-pointer h-16
-            max-sm:translate-y-12">
+            <button className="flex flex-row gap-2 justify-center items-center py-4 px-6 border
+            border-gray-900-300 font-eb-garamond translate-y-12 group cursor-pointer h-16">
             <ArrowDownLeft width={28} height={28} strokeWidth={1} />
-            <span className="group-hover:border-b group-hover:border-b-zinc-500 text-xl h-7 rounded-b-xs">
+            <span className="group-hover:border-b group-hover:border-b-zinc-400 text-xl h-7 rounded-b-xs">
                 Explorar
             </span>
         </button>
